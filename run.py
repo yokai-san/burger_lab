04aa3238f39eaac005533dfe7dac0dde64044b5c
yokai-san <chrislawler7@gmail.com>
2022-10-13 23:54:00 +0000

    Added & amended getting_toppings_list function, added pretty table receipt, Add goodbye message

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ def print_type(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.00)
+        time.sleep(0.05)
 
 
 def input_type(text):
@@ -50,7 +50,7 @@ def input_type(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.00)
+        time.sleep(0.05)
     value = input()
     return value
 
@@ -67,6 +67,11 @@ def buffer():
     Pause after warning before clearing screen
     """
     time.sleep(.5)
+
+
+now = datetime.now()
+date_stamp = now.strftime("%a:%d:%b:%y")
+time_stamp = now.strftime("%H:%M")
 
 
 # x = PrettyTable()
@@ -103,7 +108,7 @@ def get_order():
     print_type("\nWhat type of burger can I get you today?\n")
     print_type("You can select from Beef or Vegan.\n")
     # Request user input
-    type_of_burger = input_type("Please enter Beef or Vegan.\n")
+    type_of_burger = input_type("\nPlease enter Beef or Vegan.\n")
     if type_of_burger.lower() == ("beef"):
         print_type("\nGreat! You have chosen a Beef burger.\n")
         CUSTOMER_ORDER.append("Beef burger.")
@@ -128,7 +133,7 @@ def get_cheese():
     print_type("\nWould you like cheese on your burger?\n")
     cheese_on_burger = input_type("y/n\n")
     if cheese_on_burger.lower() == ("y"):
-        print_type("You have added cheese to your to burger.\n")
+        print_type("\nYou have added cheese to your to burger.\n")
         CUSTOMER_ORDER.append("Cheese")
 
 def update_worksheet(data, worksheet):
@@ -138,45 +143,99 @@ def update_worksheet(data, worksheet):
     """
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
+
+
+def getting_toppings_list():
+    global TOPPING_CHOICE
+    print_type("\nWhat toppings would you like for your burger?\n")
+
+    toppings = ["Pickle", "Onion", "Lettuce", "Tomato", "Ketchep", "Mustard", "Mayo"]
+    for count, value in enumerate(toppings, start=1):
+        print('{} - {}'.format(count, value))
+
+    choice = int(input("Please enter toppings by numbers: "))
+    
+    TOPPING_CHOICE.append(toppings[choice-1])
+    print_type(f"\nYou have added {TOPPING_CHOICE} to your burger.\n")
+    CUSTOMER_ORDER.append(TOPPING_CHOICE)
+    buffer()
+    clearScreen()
     
 
-def get_toppings_table():
+    
+# def get_toppings_table():
+#     """
+#     Pulling toppings list from Google Worksheet and
+#     Saving it in a variable so that it can be used in
+#     prettytable function.
+#     """
+#     toppings_list = []
+#     print_type("\nWhat toppings would you like for your burger?\n")
+#     for type in top_list:
+#         toppings_list.append(type)
+#     num = []  
+#     for i in range(1, 7):
+#         num.append(i)
+
+#     get_toppings_table.type = dict(zip(num, toppings_list))
+#     type_table = PrettyTable()
+#     type_table.field_names = num
+#     type_table.add_row(toppings_list)
+#     print(type_table)
+
+# def get_toppings():
+#     global SELECTED_TOPPINGS
+#     tops = []
+#     (map(int, input_type("\nPlease select toppings by using corresponding number\n")))
+#     SELECTED_TOPPINGS = [int(i)for i in tops]
+
+#     # return SELECTED_TOPPINGS
+#     CUSTOMER_ORDER.append(SELECTED_TOPPINGS)
+#     get_toppings_choice()
+
+
+
+# def get_toppings_choice():
+#     global TOPPING_CHOICE
+#     TOPPING_CHOICE = []
+#     for x in SELECTED_TOPPINGS:
+#         TOPPING_CHOICE.append(top_list[x - 1])
+
+
+def print_order():
     """
-    Pulling toppings list from Google Worksheet and
-    Saving it in a variable so that it can be used in
-    prettytable function.
+    Print order with prettytable
     """
-    toppings_list = []
-    print_type("\nWhat toppings would you like for your burger?\n")
-    for type in top_list:
-        toppings_list.append(type)
-    num = []  
-    for i in range(1, 7):
-        num.append(i)
+    receipt = PrettyTable()
+    receipt.field_names = (["**Your burger!**"])
+    receipt.add_row([f'Date: {date_stamp}'])
+    receipt.add_row([f'Time: {time_stamp}'])
+    receipt.add_row([f"Order: {CUSTOMER_ORDER}"])
+    print(receipt)
 
-    get_toppings_table.type = dict(zip(num, toppings_list))
-    type_table = PrettyTable()
-    type_table.field_names = num
-    type_table.add_row(toppings_list)
-    print(type_table)
-
-def get_toppings():
-    global SELECTED_TOPPINGS
-    tops = []
-    (map(int, input_type("\nPlease select toppings by using corresponding number\n")))
-    SELECTED_TOPPINGS = [int(i)for i in tops]
-
-    # return SELECTED_TOPPINGS
-    CUSTOMER_ORDER.append(SELECTED_TOPPINGS)
-    get_toppings_choice()
-
-
-
-def get_toppings_choice():
-    global TOPPING_CHOICE
-    TOPPING_CHOICE = []
-    for x in SELECTED_TOPPINGS:
-        TOPPING_CHOICE.append(top_list[x - 1])
+def good_bye():
+    print("""\
+   _____ _                 _                       
+  |_   _| |__   __ _ _ __ | | __                   
+    | | | '_ \ / _` | '_ \| |/ /                   
+    | | | | | | (_| | | | |   <                    
+    |_| |_| |_|\__,_|_| |_|_|\_\                   
+       | | | |/ _ \| | | |                         
+       | |_| | (_) | |_| |                         
+   __   \__, |\___/ \__,_|     _ _   _             
+  / _| _|___/ __  __   _(_)___(_) |_(_)_ __   __ _ 
+ | |_ / _ \| '__| \ \ / / / __| | __| | '_ \ / _` |
+ |  _| (_) | |     \ V /| \__ \ | |_| | | | | (_| |
+ |_|  \___/|_|      \_/ |_|___/_|\__|_|_| |_|\__, |
+     | __ ) _   _ _ __ __ _  ___ _ __        |___/ 
+     |  _ \| | | | '__/ _` |/ _ \ '__|             
+     | |_) | |_| | | | (_| |  __/ |                
+     |____/ \__,_|_| _\__, |\___|_|                
+         | |    __ _| |___/                        
+         | |   / _` | '_ \                         
+         | |__| (_| | |_) |                        
+         |_____\__,_|_.__/                         
+                        """)
 
 
 def main():
@@ -185,12 +244,15 @@ def main():
     """
     get_order()
     get_cheese()
-    get_toppings_table()
-    get_toppings()
+    getting_toppings_list()
+    print_order()
+    good_bye()
+    # get_toppings_table()
+    # get_toppings()
     # get_toppings_choice()
     # update_worksheet(CUSTOMER_ORDER, "receipt")
-    print(CUSTOMER_ORDER)
-    print(TOPPING_CHOICE)
+    # print(CUSTOMER_ORDER)
+    # print(TOPPING_CHOICE)
 
 
 main()
